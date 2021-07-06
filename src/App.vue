12e745a3c4c288ea12fa293bcf99a49580db77ea
Sora Arakawa <sr@arkw.jp>
2021-07-06 16:51:56 +0900

    Headerを追加

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <p>
-      arkw.work-vue
-    </p>
-  </div>
+	<div>
+		<Header></Header>
+	</div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default({
+	name: 'app',
+	components: {
+		Header
+	}
+})
+</script>
+
+<style scoped>
+a {
+	color: #3f51b5;
+}
+
+a:hover {
+	opacity: 0.7;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<div class="header">
+			<div class="wrapper">
+				<img src="../assets/logo.svg">
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.header {
+	padding-top: 20px;
+	padding-bottom: 8px;
+	padding-left: 40px;
+	padding-right: 40px;
+	position: relative;
+	z-index: 100;
+	background-color: #3f51b5;
+}
+.header img {
+	height: 60px;
+}
+</style>
